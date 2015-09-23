a03f7bb7cf2407c9dfb5e365add9d56b36d8ae2d
maxim467 <maxim467@gmail.com>
2015-09-23 19:26:24 +0200

    xx

--- OpenCV face-detection/jni/Android.mk
+++ OpenCV face-detection/jni/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 #OPENCV_CAMERA_MODULES:=off
 #OPENCV_INSTALL_MODULES:=off
 #OPENCV_LIB_TYPE:=SHARED
-include ../../sdk/native/jni/OpenCV.mk
+include /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
 
 LOCAL_SRC_FILES  := DetectionBasedTracker_jni.cpp
 LOCAL_C_INCLUDES += $(LOCAL_PATH)
--- OpenCV face-detection/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
+++ OpenCV face-detection/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -4,10 +4,10 @@ obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: 
  /Users/MiguelGarcia/Documents/ANDROID/android-ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
  /Users/MiguelGarcia/Documents/ANDROID/android-ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
  /Users/MiguelGarcia/Documents/ANDROID/android-ndk/platforms/android-8/arch-arm/usr/include/android/api-level.h \
- ../../sdk/native/jni/include/opencv2/core/core.hpp \
- ../../sdk/native/jni/include/opencv2/core.hpp \
- ../../sdk/native/jni/include/opencv2/core/cvdef.h \
- ../../sdk/native/jni/include/opencv2/hal/defs.h \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal/defs.h \
  /Users/MiguelGarcia/Documents/ANDROID/android-ndk/platforms/android-8/arch-arm/usr/include/limits.h \
  /Users/MiguelGarcia/Documents/ANDROID/android-ndk/platforms/android-8/arch-arm/usr/include/sys/limits.h \
  /Users/MiguelGarcia/Documents/ANDROID/android-ndk/platforms/android-8/arch-arm/usr/include/linux/limits.h \
@@ -20,38 +20,38 @@ obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: 
  /Users/MiguelGarcia/Documents/ANDROID/android-ndk/platforms/android-8/arch-arm/usr/include/machine/_types.h \
  /Users/MiguelGarcia/Documents/ANDROID/android-ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/cmath \
  /Users/MiguelGarcia/Documents/ANDROID/android-ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm \
- ../../sdk/native/jni/include/opencv2/core/version.hpp \
- ../../sdk/native/jni/include/opencv2/core/base.hpp \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp \
  /Users/MiguelGarcia/Documents/ANDROID/android-ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/climits \
- ../../sdk/native/jni/include/opencv2/core/cvstd.hpp \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp \
  /Users/MiguelGarcia/Documents/ANDROID/android-ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef \
  /Users/MiguelGarcia/Documents/ANDROID/android-ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/cstring \
  /Users/MiguelGarcia/Documents/ANDROID/android-ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/cctype \
  /Users/MiguelGarcia/Documents/ANDROID/android-ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/string \
- ../../sdk/native/jni/include/opencv2/core/ptr.inl.hpp \
- ../../sdk/native/jni/include/opencv2/hal.hpp \
- ../../sdk/native/jni/include/opencv2/core/sse_utils.hpp \
- ../../sdk/native/jni/include/opencv2/core/traits.hpp \
- ../../sdk/native/jni/include/opencv2/core/matx.hpp \
- ../../sdk/native/jni/include/opencv2/core/types.hpp \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal.hpp \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/sse_utils.hpp \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp \
  /Users/MiguelGarcia/Documents/ANDROID/android-ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/cfloat \
  /Users/MiguelGarcia/Documents/ANDROID/android-ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/vector \
- ../../sdk/native/jni/include/opencv2/core/mat.hpp \
- ../../sdk/native/jni/include/opencv2/core/bufferpool.hpp \
- ../../sdk/native/jni/include/opencv2/core/mat.inl.hpp \
- ../../sdk/native/jni/include/opencv2/core/persistence.hpp \
- ../../sdk/native/jni/include/opencv2/core/operations.hpp \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
  /Users/MiguelGarcia/Documents/ANDROID/android-ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/cstdio \
- ../../sdk/native/jni/include/opencv2/core/cvstd.inl.hpp \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp \
  /Users/MiguelGarcia/Documents/ANDROID/android-ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/complex \
- ../../sdk/native/jni/include/opencv2/core/utility.hpp \
- ../../sdk/native/jni/include/opencv2/core/core_c.h \
- ../../sdk/native/jni/include/opencv2/core/types_c.h \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
  /Users/MiguelGarcia/Documents/ANDROID/android-ndk/platforms/android-8/arch-arm/usr/include/assert.h \
- ../../sdk/native/jni/include/opencv2/core/optim.hpp \
- ../../sdk/native/jni/include/opencv2/objdetect.hpp \
- ../../sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp \
- ../../sdk/native/jni/include/opencv2/objdetect/objdetect_c.h \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect.hpp \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp \
+ /Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect_c.h \
  /Users/MiguelGarcia/Documents/ANDROID/android-ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/deque \
  /Users/MiguelGarcia/Documents/ANDROID/android-ndk/platforms/android-8/arch-arm/usr/include/android/log.h
 
@@ -65,13 +65,13 @@ jni/DetectionBasedTracker_jni.h:
 
 /Users/MiguelGarcia/Documents/ANDROID/android-ndk/platforms/android-8/arch-arm/usr/include/android/api-level.h:
 
-../../sdk/native/jni/include/opencv2/core/core.hpp:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
 
-../../sdk/native/jni/include/opencv2/core.hpp:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp:
 
-../../sdk/native/jni/include/opencv2/core/cvdef.h:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h:
 
-../../sdk/native/jni/include/opencv2/hal/defs.h:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal/defs.h:
 
 /Users/MiguelGarcia/Documents/ANDROID/android-ndk/platforms/android-8/arch-arm/usr/include/limits.h:
 
@@ -97,13 +97,13 @@ jni/DetectionBasedTracker_jni.h:
 
 /Users/MiguelGarcia/Documents/ANDROID/android-ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/algorithm:
 
-../../sdk/native/jni/include/opencv2/core/version.hpp:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
 
-../../sdk/native/jni/include/opencv2/core/base.hpp:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp:
 
 /Users/MiguelGarcia/Documents/ANDROID/android-ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/climits:
 
-../../sdk/native/jni/include/opencv2/core/cvstd.hpp:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp:
 
 /Users/MiguelGarcia/Documents/ANDROID/android-ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/cstddef:
 
@@ -113,53 +113,53 @@ jni/DetectionBasedTracker_jni.h:
 
 /Users/MiguelGarcia/Documents/ANDROID/android-ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/string:
 
-../../sdk/native/jni/include/opencv2/core/ptr.inl.hpp:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp:
 
-../../sdk/native/jni/include/opencv2/hal.hpp:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal.hpp:
 
-../../sdk/native/jni/include/opencv2/core/sse_utils.hpp:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/sse_utils.hpp:
 
-../../sdk/native/jni/include/opencv2/core/traits.hpp:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp:
 
-../../sdk/native/jni/include/opencv2/core/matx.hpp:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp:
 
-../../sdk/native/jni/include/opencv2/core/types.hpp:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp:
 
 /Users/MiguelGarcia/Documents/ANDROID/android-ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/cfloat:
 
 /Users/MiguelGarcia/Documents/ANDROID/android-ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/vector:
 
-../../sdk/native/jni/include/opencv2/core/mat.hpp:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
 
-../../sdk/native/jni/include/opencv2/core/bufferpool.hpp:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp:
 
-../../sdk/native/jni/include/opencv2/core/mat.inl.hpp:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp:
 
-../../sdk/native/jni/include/opencv2/core/persistence.hpp:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp:
 
-../../sdk/native/jni/include/opencv2/core/operations.hpp:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
 
 /Users/MiguelGarcia/Documents/ANDROID/android-ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/cstdio:
 
-../../sdk/native/jni/include/opencv2/core/cvstd.inl.hpp:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp:
 
 /Users/MiguelGarcia/Documents/ANDROID/android-ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/complex:
 
-../../sdk/native/jni/include/opencv2/core/utility.hpp:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp:
 
-../../sdk/native/jni/include/opencv2/core/core_c.h:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
 
-../../sdk/native/jni/include/opencv2/core/types_c.h:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
 
 /Users/MiguelGarcia/Documents/ANDROID/android-ndk/platforms/android-8/arch-arm/usr/include/assert.h:
 
-../../sdk/native/jni/include/opencv2/core/optim.hpp:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp:
 
-../../sdk/native/jni/include/opencv2/objdetect.hpp:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect.hpp:
 
-../../sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp:
 
-../../sdk/native/jni/include/opencv2/objdetect/objdetect_c.h:
+/Users/MiguelGarcia/Downloads/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect_c.h:
 
 /Users/MiguelGarcia/Documents/ANDROID/android-ndk/sources/cxx-stl/gnu-libstdc++/4.8/include/deque:
 
